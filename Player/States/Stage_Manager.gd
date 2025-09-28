091edc10140418d366b0d2c34e315ffd87474763
Ahmad <ahmed@apples-MacBook-Pro.local>
2025-09-28 23:17:29 +0300

    Initial commit of Godot project

---
+++ Player/States/Stage_Manager.gd
@@ -0,0 +1,16 @@
+class_name StateManager
+
+
+var states
+func _init():
+	states = {
+		"idle": IdleState,
+		"run": RunState,	
+	}
+
+
+func get_state(state_name):
+	if states.has(state_name):
+		return states.get(state_name)
+	else:
+		printerr("No State ", state_name, "Exists")	
